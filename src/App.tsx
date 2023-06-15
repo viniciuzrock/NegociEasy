cce2795de241c277719ab092c8f5abd9529144ac
viniciuzrock <vinir.santoss@gmail.com>
2023-06-14 23:05:26 -0300

    feat:configurando comunicacao entre navbar e home

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Login from './components/Form/Login/Login'
 import Home from './components/pages/Home/Home';
 import Auth from './components/pages/Auth/Auth';
 function App() {
+  const [searchValue, setSearchValue] = useState('')
+
+  const handleSearch = (value: string) => {
+    setSearchValue(value)
+    console.log(value);
+
+  }
+
   return (
     <div className="App">
       <Router>
         <Routes>
           {/* <Route path='/' element={<Login />} /> */}
           <Route path='/' element={<Auth />} />
-          <Route path='/home' element={<Navbar><Home /> </Navbar>} />
+          {/* <Route path='/home' element={
+            <Navbar onSearch={handleSearch}>
+              <Home />
+            </Navbar>}
+          /> */}
+          {/* <Route path="/home" element={<Home />} /> */}
+          <Route path="/home" element={
+            <Navbar onSearch={handleSearch} >
+              <Home searchValue={searchValue} />
+            </Navbar>
+          }
+          />
         </Routes>
-
       </Router>
     </div>
   );
